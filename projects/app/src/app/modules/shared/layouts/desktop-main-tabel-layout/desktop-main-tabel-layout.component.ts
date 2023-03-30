3d496d72517d01b57461e15f6da340e2dd34f799
deniz-DD <deniz_dural@yahoo.com>
2023-03-30 21:10:10 +0200

    init commit and use of ngFor

--- projects/app/src/app/modules/shared/layouts/desktop-main-tabel-layout/desktop-main-tabel-layout.component.ts
+++ projects/app/src/app/modules/shared/layouts/desktop-main-tabel-layout/desktop-main-tabel-layout.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./desktop-main-tabel-layout.component.scss'],
 })
 export class DesktopMainTabelLayoutComponent {
+  rows: any = [
+    {
+      nr: 160,
+      data: 'Skischuhe',
+      label: 'Addidas',
+      size: 'xl',
+      color: 'blau',
+      other: 'nix',
+      max: 160,
+      min: 150,
+    },
+    {
+      nr: 160,
+      data: 'Skischuhe',
+      label: 'Addidas',
+      size: 'xl',
+      color: 'blau',
+      other: 'nix',
+      max: 160,
+      min: 150,
+    },
+    {
+      nr: 160,
+      data: 'Skischuhe',
+      label: 'Addidas',
+      size: 'xl',
+      color: 'blau',
+      other: 'nix',
+      max: 160,
+      min: 150,
+    },
+  ];
+
   constructor() {}
 }
