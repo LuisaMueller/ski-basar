dd1ccb67d5ff4a5b0f208ecf2bcf8c6ddb6fa37d
LuisaMueller <mueller-luisa@gmx.de>
2023-07-07 20:24:26 +0200

    create content for admin-check-cash; some other changes

--- projects/app/src/app/sections/main/main.module.ts
+++ projects/app/src/app/sections/main/main.module.ts
@@ -5,11 +5,13 @@ import { RouterModule } from '@angular/router';
 import { BaseComponentsModule } from '@lenne.tech/ng-base/base-components';
 import { BasePrototypeModule } from '@lenne.tech/ng-base/base-prototype';
 import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
-import { AdminCreateUserComponent } from '../../modules/shared/layouts/admin-create-user/admin-create-user.component';
 import { SharedModule } from '../../modules/shared/shared.module';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { AdminCheckCashComponent } from './pages/admin-check-cash/admin-check-cash.component';
+import { AdminSeeArchiveComponent } from './pages/admin-see-archive/admin-see-archive.component';
+import { AdminSeeListsComponent } from './pages/admin-see-lists/admin-see-lists.component';
+import { AdminShowStatisticsComponent } from './pages/admin-show-statistics/admin-show-statistics.component';
 import { DeleteTableModalComponent } from './pages/delete-table-modal/delete-table-modal.component';
 import { DesktopGetNumberComponent } from './pages/desktop-get-number/desktop-get-number.component';
 import { DesktopMainTabelLayoutComponent } from './pages/desktop-main-tabel-layout/desktop-main-tabel-layout.component';
@@ -17,9 +19,6 @@ import { DesktopNumberLayoutComponent } from './pages/desktop-number-layout/desk
 import { MyProfileComponent } from './pages/my-profile/my-profile.component';
 import { PayoutAmountModalComponent } from './pages/payout-amount-modal/payout-amount-modal.component';
 import { UpdateTableModalComponent } from './pages/update-table-modal/update-table-modal.component';
-import { AdminSeeListsComponent } from './pages/admin-see-lists/admin-see-lists.component';
-import { AdminSeeArchiveComponent } from './pages/admin-see-archive/admin-see-archive.component';
-import { AdminShowStatisticsComponent } from './pages/admin-show-statistics/admin-show-statistics.component';
 
 @NgModule({
   declarations: [
@@ -31,7 +30,6 @@ import { AdminShowStatisticsComponent } from './pages/admin-show-statistics/admi
     PayoutAmountModalComponent,
     DesktopGetNumberComponent,
     DesktopNumberLayoutComponent,
-    AdminCreateUserComponent,
     AdminCheckCashComponent,
     AdminSeeListsComponent,
     AdminSeeArchiveComponent,
